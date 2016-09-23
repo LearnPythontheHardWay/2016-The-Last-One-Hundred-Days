9340e28d23b8b178b9af41b1b49be17cbca93a68
awthink <github.hihr@yidie.org>
2016-09-24 00:30:15 +0800

    ex2

---
+++ ex2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# A comment,this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print "I could have code like this." #and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run."
+
+print "This will run."
+
+# 倒着读是为了找拼写错误吗？
+# 找拼写错误我可不太在行……
