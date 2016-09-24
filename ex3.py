fe0b3e5c492be7b984d66f300402e4b5951263cb
awthink <github.hihr@yidie.org>
2016-09-24 20:06:07 +0800

    ex3

---
+++ ex3.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# 计算鸡
+print "I will now count my chickens:"
+
+# 母鸡价格
+print "Hens", 25+30/6
+# 公鸡价格
+print "Roosters", 100-25*3%4
+
+# 计算蛋
+print "Now I will count the eggs:"
+
+print 3+2+1-5+4%2-1/4+6
+
+# ……编不下去了
+print "Is it true that 3+2 < 5-7 ?"
+
+print 3+2<5-7
+
+print "What is 3+1 ?", 3+2
+print "What is 5-7 ?", 5-7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+# 左边大于右边吗
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <=-2
