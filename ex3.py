bcd0c66c4923f35d9ff1270c951ba981cb5b5d9b
awthink <github.hihr@yidie.org>
2016-09-25 10:41:53 +0800

    ex4

--- ex3.py
+++ ex3.py
@@ -16,7 +16,7 @@ print 3+2+1-5+4%2-1/4+6
 # ……编不下去了
 print "Is it true that 3+2 < 5-7 ?"
 
-print 3+2<5-7
+print 3+2 < 5-7
 
 print "What is 3+1 ?", 3+2
 print "What is 5-7 ?", 5-7
@@ -28,4 +28,4 @@ print "How about some more."
 # 左边大于右边吗
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
-print "Is it less or equal?", 5 <=-2
+print "Is it less or equal?", 5 <= -2
